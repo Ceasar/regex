6eccbce4a2c8b6c423fb4866d6524efecc180cf1
Adi Dahiya <adidas006@gmail.com>
2012-04-07 21:32:27 -0400

    initial commit

---
+++ DFA.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS_GHC -Wall #-}
+
+{- Deterministic Finite Automaton
+ - CIS 194
+ - Ceasar Bautista, Adi Dahiya
+ -}
+
+
+module DFA where
+
+{- DFA M is defined by
+ -- finite set of states (Q)
+ -- finite set of input symbols called the alphabet (A)
+ -- transition function (D)
+ -- start state (Q)
+ -- set of accept states (F)
+ -}
+
+
