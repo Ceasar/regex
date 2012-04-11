7e76ff88a940aa78f8102671ee2428820011cb6b
Ceasar Bautista <cbautista2010@gmail.com>
2012-04-11 00:51:33 -0400

    Added some test functions.

--- DFA.hs
+++ DFA.hs
@@ -22,3 +22,14 @@ data DFA = DFA [State] [Char] (State -> Char -> State) State [State]
 run :: DFA -> String -> Bool
 run (DFA _ _ _ q fs) [] = q `elem` fs
 run (DFA qs a d q fs) (x:xs) = run (DFA qs a d (d q x) fs) xs
+
+-- Test functions
+
+testTransition :: State -> Char -> State
+testTransition 1 'a' = 1
+testTransition 1 'b' = 2
+testTransition 2 'a' = 2
+testTransition 2 'b' = 1
+
+testDFA :: DFA
+testDFA = DFA [1, 2] "ab" testTransition 1 [2]
