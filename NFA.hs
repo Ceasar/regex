03b9c80f486acf481f332ccb0e79c2406b6f91cf
Ceasar Bautista <cbautista2010@gmail.com>
2012-10-23 17:51:25 -0400

    Remove Moves
    Because they are a list, they slow down the NFA. NFA's need a function. (A map
    would not suffice because it's possible a key might not be defined.)

--- NFA.hs
+++ NFA.hs
@@ -1,20 +1,18 @@
-module NFA (Nfa(NFA), Move(Move, Emove), run, accepts, nfaUnion, nfaConcat,
-    nfaStar) where
+module NFA (Nfa(NFA), run, accepts, {-nfaUnion, nfaConcat, nfaStar-}) where
 
 
 import Data.Set as S
 
+type State = Int
 
-data Nfa a = NFA (Set a) (Set (Move a)) a (Set a)
-    deriving (Eq, Show)
-
-data Move a = Move a Char a
-            | Emove a a
-    deriving (Eq, Ord, Show)
+data Nfa = NFA  (Set State) -- Set of states
+                (Maybe Char -> State -> Set State) -- Relation
+                State -- Starting state
+                (Set State) -- Final states
 
 
 -- | Keep iterating until a static set remains
-setlimit :: Eq a => (Set a -> Set a) -> Set a -> Set a
+setlimit :: (Set State -> Set State) -> Set State -> Set State
 setlimit f s
     | s == next = s
     | otherwise = setlimit f next
@@ -23,47 +21,34 @@ setlimit f s
 
 
 -- | Compute the set of states reachable from epsilon transitions
-closure :: Ord a => Nfa a -> Set a -> Set a
-closure (NFA _ moves _ _) = setlimit add
-  where
-    add states = states `union` accessible
-      where
-        accessible = fromList [s |  x <- toList states,
-                                    Emove y s <- toList moves,
-                                    y == x]
+closure :: Nfa -> Set State -> Set State
+closure (NFA _ f _ _) qs = setlimit (S.fold (S.union . f Nothing) qs) qs
 
 
 -- | Compute the set of states reachable from symbolic transitions
-onemove :: Ord a => Nfa a -> Char -> Set a -> Set a
-onemove (NFA _ ms _ _) c x = fromList [s |  t <- toList x,
-                                            Move z d s <- toList ms,
-                                            z == t, c == d]
+onemove :: Nfa -> Char -> Set State -> Set State
+onemove (NFA _ f _ _) c = S.fold (S.union . f (Just c) ) S.empty
 
 
 -- | Compute the state of the machine after a single transition
-onetrans :: Ord a => Nfa a -> Set a -> Char -> Set a
+onetrans ::  Nfa -> Set State -> Char -> Set State
 onetrans m x c = closure m (onemove m c x)
 
 
 -- | Compute the final state of an NFA on a string
-run :: Ord a => Nfa a -> String -> Set a
+run :: Nfa -> String -> Set State
 run m@(NFA _ _ q _) = Prelude.foldl (onetrans m) (closure m (singleton q))
 
 
 -- | Check if a NFA accepts a word
-accepts :: Ord a => Nfa a -> String -> Bool
+accepts :: Nfa -> String -> Bool
 accepts m@(NFA _ _ _ fs) = not . S.null . S.intersection fs . run m
 
 
--- | Map a transition to a larger graph
-renumberMove :: (Num a) => a -> Move a -> Move a
-renumberMove i (Move a c b) = Move (a + i) c (b + i)
-renumberMove i (Emove a b)  = Emove (a + i) (b + i)
-
-
+{-
 -- | Combine two NFAs which recognize languages L1 and L2 to form a new
 -- NFA which recognizes L1 | L2.
-nfaUnion :: Nfa Int -> Nfa Int -> Nfa Int
+nfaUnion :: Nfa -> Nfa -> Nfa
 nfaUnion (NFA states1 moves1 _ _) (NFA states2 moves2 _ _)
     = NFA (fromList [0..(m1+m2+1)])
           (moves1' `union` moves2' `union` newmoves)
@@ -82,7 +67,7 @@ nfaUnion (NFA states1 moves1 _ _) (NFA states2 moves2 _ _)
 
 -- | Combine two NFAs which recognize languages L1 and L2 to form a new
 -- NFA which recognizes L1 + L2.
-nfaConcat :: Nfa Int -> Nfa Int -> Nfa Int
+nfaConcat :: Nfa -> Nfa -> Nfa
 nfaConcat (NFA states1 moves1 _ _) (NFA states2 moves2 _ _)
     = NFA (fromList [0..(m1 + m2 - 1)])
           (moves1' `union` moves2' `union` newmoves)
@@ -98,7 +83,7 @@ nfaConcat (NFA states1 moves1 _ _) (NFA states2 moves2 _ _)
 
 -- | Transform an NFA which recognizes language L1 to a new NFA
 -- which recognizes L1*
-nfaStar :: Nfa Int -> Nfa Int
+nfaStar :: Nfa -> Nfa
 nfaStar (NFA states1 moves1 _ _)
     = NFA (fromList [0..(m1 + 1)])
           (moves1' `union` newmoves)
@@ -111,3 +96,4 @@ nfaStar (NFA states1 moves1 _ _)
                                  Emove 0 (m1 + 1),
                                  Emove m1 1,
                                  Emove m1 (m1 + 1)]
+-}
