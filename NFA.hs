736d582811569587c550d5cb93a2e7d04f742418
Ceasar Bautista <cbautista2010@gmail.com>
2012-10-23 15:26:33 -0400

    style changes

--- NFA.hs
+++ NFA.hs
@@ -1,4 +1,5 @@
-module NFA (Nfa(NFA), Move(Move, Emove), run, accepts, nfaUnion, nfaConcat, nfaStar) where
+module NFA (Nfa(NFA), Move(Move, Emove), run, accepts, nfaUnion, nfaConcat,
+    nfaStar) where
 
 
 import Data.Set as S
@@ -25,14 +26,18 @@ setlimit f s
 closure :: Ord a => Nfa a -> Set a -> Set a
 closure (NFA _ moves _ _) = setlimit add
   where
-    add states = union states accessible
+    add states = states `union` accessible
       where
-        accessible = fromList [s | x <- toList states, Emove y s <- toList moves, y == x]
+        accessible = fromList [s |  x <- toList states,
+                                    Emove y s <- toList moves,
+                                    y == x]
 
 
 -- | Compute the set of states reachable from symbolic transitions
 onemove :: Ord a => Nfa a -> Char -> Set a -> Set a
-onemove (NFA _ ms _ _) c x = fromList [s | t <- toList x, Move z d s <- toList ms, z == t, c == d]
+onemove (NFA _ ms _ _) c x = fromList [s |  t <- toList x,
+                                            Move z d s <- toList ms,
+                                            z == t, c == d]
 
 
 -- | Compute the state of the machine after a single transition
@@ -47,24 +52,19 @@ run m@(NFA _ _ q _) = Prelude.foldl (onetrans m) (closure m (singleton q))
 
 -- | Check if a NFA accepts a word
 accepts :: Ord a => Nfa a -> String -> Bool
-accepts m@(NFA qs ms q fs) = not . S.null . S.intersection fs . run m
-
-
--- | Map a state to a new graph
-renumber :: (Num a) => a -> a -> a
-renumber = (+)
+accepts m@(NFA _ _ _ fs) = not . S.null . S.intersection fs . run m
 
 
 -- | Map a transition to a larger graph
-renumber_move :: (Num a) => a -> Move a -> Move a
-renumber_move i (Move a c b) = (Move (a + i) c (b + i))
-renumber_move i (Emove a b)  = (Emove (a + i) (b + i))
+renumberMove :: (Num a) => a -> Move a -> Move a
+renumberMove i (Move a c b) = Move (a + i) c (b + i)
+renumberMove i (Emove a b)  = Emove (a + i) (b + i)
 
 
 -- | Combine two NFAs which recognize languages L1 and L2 to form a new
 -- NFA which recognizes L1 | L2.
 nfaUnion :: Nfa Int -> Nfa Int -> Nfa Int
-nfaUnion (NFA states1 moves1 start1 finish1) (NFA states2 moves2 start2 finish2)
+nfaUnion (NFA states1 moves1 _ _) (NFA states2 moves2 _ _)
     = NFA (fromList [0..(m1+m2+1)])
           (moves1' `union` moves2' `union` newmoves)
           0
@@ -72,8 +72,8 @@ nfaUnion (NFA states1 moves1 start1 finish1) (NFA states2 moves2 start2 finish2)
           where
             m1 = size states1
             m2 = size states2
-            moves1'  = mapMonotonic (renumber_move 1)        moves1
-            moves2'  = mapMonotonic (renumber_move (m1 + 1)) moves2
+            moves1'  = mapMonotonic (renumberMove 1)        moves1
+            moves2'  = mapMonotonic (renumberMove (m1 + 1)) moves2
             newmoves = fromList [Emove 0 1,
                                 Emove 0 (m1 + 1),
                                 Emove m1 (m1 + m2 + 1),
@@ -83,7 +83,7 @@ nfaUnion (NFA states1 moves1 start1 finish1) (NFA states2 moves2 start2 finish2)
 -- | Combine two NFAs which recognize languages L1 and L2 to form a new
 -- NFA which recognizes L1 + L2.
 nfaConcat :: Nfa Int -> Nfa Int -> Nfa Int
-nfaConcat (NFA states1 moves1 start1 finish1) (NFA states2 moves2 start2 finish2)
+nfaConcat (NFA states1 moves1 _ _) (NFA states2 moves2 _ _)
     = NFA (fromList [0..(m1 + m2 - 1)])
           (moves1' `union` moves2' `union` newmoves)
           0
@@ -92,21 +92,21 @@ nfaConcat (NFA states1 moves1 start1 finish1) (NFA states2 moves2 start2 finish2
             m1 = size states1
             m2 = size states2
             moves1'  = moves1
-            moves2'  = mapMonotonic (renumber_move m1) moves2
+            moves2'  = mapMonotonic (renumberMove m1) moves2
             newmoves = singleton (Emove (m1 - 1) m1)
 
 
 -- | Transform an NFA which recognizes language L1 to a new NFA
 -- which recognizes L1*
 nfaStar :: Nfa Int -> Nfa Int
-nfaStar (NFA states1 moves1 start1 finish1)
+nfaStar (NFA states1 moves1 _ _)
     = NFA (fromList [0..(m1 + 1)])
           (moves1' `union` newmoves)
           0
           (fromList [m1 + 1])
           where
             m1 = size states1
-            moves1'  = mapMonotonic (renumber_move 1) moves1
+            moves1'  = mapMonotonic (renumberMove 1) moves1
             newmoves = fromList [Emove 0 1,
                                  Emove 0 (m1 + 1),
                                  Emove m1 1,
